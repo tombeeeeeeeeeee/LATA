2bf0eab3effc77cd6723b51c07fa26a79e2adb95
Lochlan McDonald <lochlanpmc@gmail.com>
2026-02-17 18:10:46 +1100

    Cleared test scene

--- LochieGraphics/TestScene.h
+++ LochieGraphics/TestScene.h
@@ -1,45 +1,9 @@
 #pragma once
 #include "Scene.h"
 
-#include "Lights.h"
-#include "MessengerInterface.h"
-#include "Animator.h"
-#include "BlendedAnimator.h"
-#include "Animation.h"
-
 class TestScene : public Scene
 {
 private:
-	MessengerInterface messengerInterface;
-
-	Shader* uiShader = nullptr;
-
-	int skyboxIndex = 0;
-	std::vector<Skybox*> skyboxes;
-
-	SceneObject* backpack;
-	SceneObject* soulSpear;
-	SceneObject* testRedBox;
-	SceneObject* grass;
-	SceneObject* boxes;
-	SceneObject* lightCube;
-	SceneObject* xbot;
-	SceneObject* vampire;
-	SceneObject* aniTest;
-	SceneObject* tires;
-
-	Animation* xbotChicken;
-	Animation* xbotIdle;
-	Animator xbotAnimator;
-	Animator xbotOtherAnimator;
-	BlendedAnimator xbotBlendedAnimator;
-	float lerpAmount = 0.f;
-
-	Animation* vampireWalk;
-	Animator vampireAnimator;
-
-	
-
 
 public:
 
